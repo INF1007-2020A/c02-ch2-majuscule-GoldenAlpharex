6bc4e65259b2d00486a8a0b94199d196d829f224
GoldenAlpharex <jerego1234@hotmail.com>
2020-09-10 22:10:03 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    distance_entre_min_et_maj = -32
+    distance_entre_min_et_maj = ord(a) - ()
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
